169ca9592c92a926480db2e94dfecf4ea8ba9750
Jonathan Backhaus <jonathan.david.backhaus@gmail.com>
2019-10-04 21:13:52 -0400

    Working baseline

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+DOCKER_VOLUME_VARKEN=docker-varken_config-varken
+DOCKER_VOLUME_GRAFANA=docker-varken_config-grafana
+
+# Spin up services first time
+docker-compose up -d influxdb
+docker-compose up -d varken
+
+# Replace varken.ini file
+docker stop varken
+docker cp varken.ini varken:/config/
+docker-compose up -d varken
+
+# Fix permissions for grafana
+docker-compose up -d grafana
+docker stop grafana
+docker run -t -v $DOCKER_VOLUME_GRAFANA:/config --name helper busybox chown -R 472:472 /config
+docker-compose up -d grafana
+
+# Import the official grafana dashboard
+docker cp utilities/grafana_build.py varken:/config/grafana_build.py
+docker exec -t varken python3 /config/grafana_build.py
