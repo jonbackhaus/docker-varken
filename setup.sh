13aa7bbea61aabbbe07c55f9772d66e5e71016db
Jonathan Backhaus <jonathan.david.backhaus@gmail.com>
2019-10-05 17:09:47 -0400

    Fixed script errors

--- setup.sh
+++ setup.sh
@@ -13,9 +13,9 @@ docker-compose up -d varken
 # Fix permissions for grafana
 docker-compose up -d grafana
 docker stop grafana
-docker run -t -v $DOCKER_VOLUME_GRAFANA:/config --name helper busybox chown -R 472:472 /config
+docker run -t --rm -v $DOCKER_VOLUME_GRAFANA:/config busybox chown -R 472:472 /config
 docker-compose up -d grafana
 
 # Import the official grafana dashboard
 docker cp utilities/grafana_build.py varken:/config/grafana_build.py
-docker exec -t varken python3 /config/grafana_build.py
+docker exec -it varken python3 /config/grafana_build.py
